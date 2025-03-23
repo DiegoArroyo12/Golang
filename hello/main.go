12cf96a77d8377f299a5b97de1b79acd93ff7f4c
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2025-03-23 14:54:22 -0600

    Devolver y manejar un error

--- hello/main.go
+++ hello/main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/DiegoArroyo12/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Diego")
+	// Agregamos el prefijo a los errores y ocultamos fecha y hora del error
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
