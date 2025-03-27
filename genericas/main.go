5cda90cc1b4bea72ccb61fea91aaa7968d2fd52f
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2025-03-27 13:43:28 -0600

    Parámetros de tipos y restricciones

--- genericas/main.go
+++ genericas/main.go
@@ -8,8 +8,27 @@ func PrintList(list ...any) {
 	}
 }
 
+type integer int 
+
+// Restricción arbitraria
+func Sum[T ~int | ~float64](nums ...T) T {
+	var total T
+	for _, num := range nums {
+		total += num
+	}
+	return total
+}
+
 func main() {
 	PrintList("Diego", "Alex", "Emmanuel", "Ricardo")
 	PrintList(100, 456, 789, 456, 247)
 	PrintList("Hola", 452, 4.25, true)
+
+	fmt.Println(Sum[int](4, 5, 6, 4))
+	fmt.Println(Sum(4.5, 5.5, 6.5, 4.5))
+
+	var num1 integer = 100
+	var num2 integer = 300
+
+	fmt.Println(Sum(num1, num2))
 }
