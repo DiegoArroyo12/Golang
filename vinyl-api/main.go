c13d7ba2a5ff5d77d5b6347c8868f0fe66b11c75
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2025-03-30 00:17:17 -0600

    Controlador para agregar un nuevo elemento

--- vinyl-api/main.go
+++ vinyl-api/main.go
@@ -7,10 +7,10 @@ import (
 )
 
 type album struct {
-	ID string `json: "id"`
-	Title string `json: "title"`
-	Artist string `json: "artist"`
-	Price float64 `json: "price"`
+	ID string `json:"id"`
+	Title string `json:"title"`
+	Artist string `json:"artist"`
+	Price float64 `json:"price"`
 }
 
 var albums = []album {
@@ -23,10 +23,23 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+
+	c.IndentedJSON(http.StatusCreated, albums)
+}
+
 func main() {
 	router := gin.Default()
 
 	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
 }
