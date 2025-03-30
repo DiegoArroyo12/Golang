c2522611e5b716e0a1e862a54ba97cc4db960fb4
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2025-03-30 00:08:27 -0600

    Controlador para devolver todos los elementos

--- vinyl-api/main.go
+++ vinyl-api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,17 +16,17 @@ type album struct {
 var albums = []album {
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "John Coltrane", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 90.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
 }
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H {
-			"message": "Hola Mundo",	
-		})
-	})
+	router.GET("/albums", getAlbums)
 
 	router.Run("localhost:8080")
 }
