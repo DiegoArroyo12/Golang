63e394c5ce243d5152c7ebc006c7df6100e1e689
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2025-04-01 09:39:51 -0600

    Uso de godotenv

--- go-mysql/main.go
+++ go-mysql/main.go
@@ -1,23 +1,18 @@
 package main
 
 import (
-	"database/sql"
+	"go-mysql/database"
 	"log"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	dns := "root:rootmac24@tcp(localhost:3306)/db_contacts"
-
-	// Abrir una conexión a la base de datos
-	db, err := sql.Open("mysql", dns)
+	// Establecer conexión a la base de datos
+	db, err := database.Connect()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if err := db.Ping(); err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("Conexión Exitosa a la Base de Datos")
+	defer db.Close()
 }
