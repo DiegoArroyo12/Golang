5c41338f4a936c425aed3cd956587c4ca863079b
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2025-04-15 21:07:20 -0600

    Testing Convencional

--- testing/testunitario/mate_test.go
+++ testing/testunitario/mate_test.go
@@ -2,13 +2,13 @@ package testunitario
 
 import "testing"
 
-func TestSuma(t *testing.T) {
+/* func TestSuma(t *testing.T) {
 	total := Suma(5, 5)
 
 	if total != 11 {
 		t.Errorf("Suma incorrecta, tiene %d se esperaba %d", total, 11)
 	}
-}
+} */
 /* 
 go test
 
@@ -18,3 +18,23 @@ FAIL
 exit status 1
 FAIL    github.com/DiegoArroyo12/testing/testunitario   0.229s
 */
+
+func TestSuma(t *testing.T) {
+	tabla := []struct {
+		a int
+		b int
+		c int
+	} {
+		{1,2,3},
+		{2,2,4},
+		{25,25,50},
+	}
+
+	for _, item := range tabla {
+		total := Suma(item.a, item.b)
+
+		if total != item.c {
+			t.Errorf("Suma incorrecta, tiene %d se esperaba %d", total, item.c)
+		}
+	}
+}
