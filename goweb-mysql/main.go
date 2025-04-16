37890580fa17301ed6eab9a1b50009b08ef6ff8d
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2025-04-16 13:45:17 -0600

    Insertar Registro

--- goweb-mysql/main.go
+++ goweb-mysql/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"gowebsql/db"
+	"gowebsql/models"
 )
 
 func main() {
@@ -10,7 +11,9 @@ func main() {
 	
 	fmt.Println(db.ExistsTable("users"))
 	//db.CreateTable(models.UserSchema, "users")
-	db.TruncateTable("users")
+	user := models.CreateUser("diego", "d123", "diego@gmail.com")
+	fmt.Println(user)
 
+	//db.TruncateTable("users")
 	db.Close()
 }
--- goweb-mysql/models/users.go
+++ goweb-mysql/models/users.go
@@ -1,5 +1,7 @@
 package models
 
+import "gowebsql/db"
+
 type User struct {
 	Id int
 	Username string
@@ -13,3 +15,22 @@ const UserSchema string = `CREATE TABLE IF NOT EXISTS users (
 	password VARCHAR(100) NOT NULL,
 	email VARCHAR(50),
 	create_data TIMESTAMP DEFAULT CURRENT_TIMESTAMP)`
+
+// Construir usuario
+func NewUser(username, password, email string) *User {
+	user := &User{Username: username, Password: password, Email: email}
+	return user
+}
+
+// Crear usuario e Insertar a BD
+func CreateUser(username, password, email string) *User {
+	user := NewUser(username, password, email)
+	user.insert()
+	return user
+}
+
+// Insertar Registro
+func (user *User) insert() {
+	sql := "INSERT INTO users SET username=?, password=?, email=?"
+	db.Exec(sql, user.Username, user.Password, user.Email)
+}
