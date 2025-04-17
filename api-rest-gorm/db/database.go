ad0a4e990cefa6628869d6b9cbf16b8a5390da4e
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2025-04-16 20:45:14 -0600

    Instalando GORM

---
+++ api-rest-gorm/db/database.go
@@ -0,0 +1,87 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// username:password@tcp(localhost:3306)/database
+const url = "root:rootmac24@tcp(127.0.0.1:3306)/goweb_db"
+
+// Guarda la conexión
+var db *sql.DB
+
+// Realiza la conexión
+func Connect() {
+	conection, err := sql.Open("mysql", url)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Conexión Exitosa")
+	db = conection
+}
+
+// Cierra la conexión
+func Close() {
+	db.Close()
+}
+
+// Verificar la conexión
+func Ping() {
+	if err := db.Ping(); err != nil {
+		panic(err)
+	}
+}
+
+// Verificar si una tabla existe o no
+func ExistsTable(tableName string) bool {
+	sql := fmt.Sprintf("SHOW TABLES LIKE '%s'", tableName)
+	rows, err := db.Query(sql)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	return rows.Next()
+}
+
+// Crea una tabla
+func CreateTable(schema string, name string) {
+	if !ExistsTable(name) {
+		_, err := db.Exec(schema)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+// Reiniciar el registro de una tabla
+func TruncateTable(tablename string) {
+	sql := fmt.Sprintf("TRUNCATE %s", tablename)
+	Exec(sql)
+}
+
+// Polimorfismo de Exec
+func Exec(query string, args ...interface{}) (sql.Result, error) {
+	Connect()
+	result, err := db.Exec(query, args...)
+	Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return result, err
+}
+
+// Polimorfismo de Exec
+func Query(query string, args ...interface{}) (*sql.Rows, error) {
+	Connect()
+	rows, err := db.Query(query, args...)
+	Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return rows,err
+}
